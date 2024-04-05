41e892b8e4a8894a26c9183dcbef259f2cb3ecba
michaelj451 <mike@mikeferguson.us>
2024-04-04 20:23:34 -0500

    test

---
+++ test.groovy
@@ -0,0 +1,122 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Verify Branch') {
+            steps {
+                echo "$GIT_BRANCH"
+            }
+        }
+        stage('Docker Test') {
+            steps {
+                sh(script: 'docker images -a')
+            }
+        }
+        stage('Docker Prune') {
+            steps {
+                sh(script: 'docker system prune -a -f')
+            }
+        }
+        stage('Docker image cleanup if name is already in use') {
+            steps {
+                sh(script: """
+                    if docker images -q jenkins-pipeline | grep -q .; then
+                        echo "Image already exists, removing it"
+                        docker rmi jenkins-pipeline || true
+                    else
+                        echo "Image does not exist, no action required"
+                    fi
+                    docker images -a
+                """)
+            }
+        }
+        stage('Docker Build') {
+            steps {
+                sh(script: """
+                    cd azure-vote/
+                    docker images -a
+                    docker build -t jenkins-pipeline .
+                    docker images -a
+                    cd ..
+                """)
+            }
+        }
+        stage('Check Test App') {
+            steps {
+                sh(script: """
+                    # Start the app
+                    echo "Starting test app"
+                """)
+            }
+            post {
+                success {
+                    echo 'Test app started successfully'
+                }
+                failure {
+                    echo 'Failed to start test app'
+                }
+            }
+        }
+        stage('Start Test App') {
+            steps {
+                sh(script: """
+                    # Run tests
+                    docker-compose up -d
+                    docker container ls
+                """)
+            }
+            post {
+                success {
+                    echo 'Started test app successfully'
+                }
+                failure {
+                    echo 'Failed to start test app'
+                }
+            }
+        }
+        stage('Stop Test App') {
+            steps {
+                sh(script: """
+                    # Stop the app
+                    docker-compose stop
+                    docker container ls
+                """)
+            }
+            post {
+                success {
+                    echo 'Stopped test app successfully'
+                }
+                failure {
+                    echo 'Failed to stop test app'
+                }
+            }
+        }
+        // stage('Docker Prune') {
+        //     steps {
+        //         sh(script: 'docker system prune -a -f')
+        //     }
+        // }
+        stage('Docker Push') {
+            environment {
+                // Credentials are for gitlab job credentials named gitlab_credentials
+                DOCKER_REGISTRY = 'gitlab.mikeferguson.us:5050/mxferguson/azure-vote'
+                DOCKER_IMAGE = 'jenkins-pipeline'
+                DOCKER_TAG = "${env.BUILD_NUMBER}"
+            }
+
+            steps {
+                script {
+                    echo "$DOCKER_REGISTRY"
+                    echo "$DOCKER_IMAGE"
+                    echo "$DOCKER_TAG"
+                    withCredentials([usernamePassword(credentialsId: 'gitlab_credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+                        sh "docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY"
+                        sh "docker tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_IMAGE:$DOCKER_TAG"
+                        sh "docker push $DOCKER_REGISTRY/$DOCKER_IMAGE:$DOCKER_TAG"
+                    }
+                    echo "Pushing image to registry"
+                }
+            }
+        }
+    }
+}
